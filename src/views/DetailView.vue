1b3fb02cce0460b1eef33844b13dad865dd711eb
Saeedeh Karimi <saeedehkarimi1994@gmail.com>
2025-02-08 23:19:49 +0100

    updated

--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -1,18 +1,39 @@
+<script setup>
+import { ref } from 'vue';
+
+
+const getInformationMovie = async ()=>{
+   const information = ref(null);
+    const response= await fetch("https://moviesapi.codingfront.dev/api/v1/movies/1");
+    if(response.ok){
+        const result = await response.json();
+        information.value  = result;
+    }else {
+        return;
+    }
+};
+getInformationMovie();
+
+
+</script>
+
+
+
+
 <template>
     <div class="container">
         <div class="flash_back">
             <div class="vector"><img src="/public/angle-left.svg"/></div>
             <div class="flex_detail">
                 <div class="movie_image">Pic</div>
-                <div>Details</div>
+                <div v-if="information">Details:
+
+                    {{ information }}
+                </div>
 
             </div>
 
         </div>
-
-
-
-
     </div>
     
 </template>
