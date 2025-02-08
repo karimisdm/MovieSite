67c015cc996502fcce71b6e9ddcc0bf1f005458b
Saeedeh Karimi <saeedehkarimi1994@gmail.com>
2025-02-08 23:08:50 +0100

    updated

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ListView from '@/views/ListView.vue'
+import DetailView from '@/views/DetailView.vue'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/list',
       name: 'list',
       component: ListView,
+    },
+    {
+      path: '/detail',
+      name: 'detail',
+      component: DetailView
     }
   ],
 })
---
+++ src/views/DetailView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <div class="flash_back">
+            <div class="vector"><img src="/public/angle-left.svg"/></div>
+            <div class="flex_detail">
+                <div class="movie_image">Pic</div>
+                <div>Details</div>
+
+            </div>
+
+        </div>
+
+
+
+
+    </div>
+    
+</template>
+<style scoped>
+.flesh_back {
+    width: 100%;
+}
+.flex_detail {
+    display: flex;
+    justify-content: space-between;
+}
+</style>
