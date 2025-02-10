596eadb2afa2f23f62a8dad7d9c74b152d715c9b
Saeedeh Karimi <saeedehkarimi1994@gmail.com>
2025-02-10 14:58:13 +0100

    updated

--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -3,7 +3,7 @@ import { ref } from 'vue';
 
 const information = ref(null);
 const getInformationMovie = async ()=>{
-    const response= await fetch("https://moviesapi.codingfront.dev/api/v1/movies/1");
+    const response= await fetch("https://moviesapi.codingfront.dev/api/v1/movies/3");
     if(response.ok){
         const result = await response.json();
         information.value  = result;
@@ -20,15 +20,22 @@ const getInformationMovie = async ()=>{
             <div class="vector"><img src="/public/angle-left.svg"/></div>
             <div v-if="information" class="flex_detail">
                 <div class="movie_image">  
-                    <img :src="information.images" alt="pic of movie" width="200px"/>
+                    <img :src="information.images" alt="pic of movie"/>
                     <div class="flex">
-                        <div>circle-</div>
-                        <div>rating</div>
+                        <div class="circle_score">{{ information.imdb_rating }}</div>
+                        <div>{{ information.imdb_votes }}</div>
                     </div>
                     <p>on Metacritic</p>
                 </div>
-                <div class="details">
-                    {{ information.title }} 
+                <div v-if="information" class="details">
+                   <strong class="movie_title">{{ information.title }} </strong> 
+                   <small>{{ information.genre }}</small><br/>
+                   <p class="movie_description">{{ information.plot }}</p>
+                   <ul class="flex_detail">
+                    <li class="movie_history">{{ information.rated }}</li>
+                    <li class="movie_history">{{ information.released }}</li>
+                    <li class="movie_history">{{ information.runtime }}</li>
+                   </ul>
                 </div>
             </div>
             <br/>
@@ -51,9 +58,28 @@ const getInformationMovie = async ()=>{
     background-color: blue;
     padding-left: 25px;
 }
+.details ul {
+    width: 40%;
+}
+.movie_title {
+    font-weight: 700;
+    font-size: 48px;
+    line-height: 58.09px;
+}
 .movie_image img{
-    width: 208;
-    height: 312;
-    border-radius: 18px;
+   width: 208px;
+   height: 312px;
+   border-radius: 18px;
+}
+.movie_history {
+    border-radius: 8px;
+    padding: 6px 12px;
+    background-color: #222C4F;
+    margin-top: 10px;
+    margin-bottom: 10px;
+}
+.movie_description {
+    margin-top: 15px;
+    margin-bottom: 15px;
 }
 </style>
