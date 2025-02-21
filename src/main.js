1c298a3c21d65babb3438ba722fd115a6a6c7671
Saeedeh Karimi <saeedehkarimi1994@gmail.com>
2025-02-21 20:57:14 +0100

    add pinia to project

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
 
 
+const store = createPinia();
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(store).mount('#app')
