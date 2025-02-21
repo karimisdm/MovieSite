ce8cbe67fb5df73a8d341fa696f58a82b16022c6
Saeedeh Karimi <saeedehkarimi1994@gmail.com>
2025-02-21 23:03:12 +0100

    updated

---
+++ src/stores/fetchData.js
@@ -0,0 +1,22 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+
+
+const useFetchDataStore = defineStore("",()=>{
+
+  const genres = ref(null);
+  const getMovieGenres = async ()=>{
+        const response = await fetch("https://moviesapi.codingfront.dev/api/v1/genres");
+        if(response.ok){
+            const result = await response.json();
+            genres.value = result;
+        }else{
+            return;
+            
+        }
+    };
+}
+
+)
+
